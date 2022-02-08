ea6ffb41c2cafc15f3530cef6de409ee4e21f8ad
devlopper <kycdev@gmail.com>
2022-02-08 13:58:47 +0000

    commit

--- test/test/src/main/java/org/cyk/quarkus/extension/test/Profile.java
+++ test/test/src/main/java/org/cyk/quarkus/extension/test/Profile.java
@@ -1,5 +1,6 @@
 package org.cyk.quarkus.extension.test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -11,17 +12,17 @@ import org.apache.commons.lang3.StringUtils;
 
 public interface Profile {
 
-	public static Set<String> buildTags(Set<Class<?>> classes) {
-		Set<String> tags = classes.stream().map(klass -> klass.getSimpleName().toLowerCase()).collect(Collectors.toSet());
+	public static Set<String> buildTags(List<Class<?>> classes) {
+		List<String> tags = classes.stream().map(klass -> klass.getSimpleName().toLowerCase()).collect(Collectors.toList());
 		tags.add(StringUtils.join(tags, "."));
-		return tags;
+		return new LinkedHashSet<String>(tags);
 	}
 	
 	public static Set<String> buildTags(Class<?>...classes) {
-		return buildTags(classes == null || classes.length == 0 ? null : Set.of(classes));
+		return buildTags(classes == null || classes.length == 0 ? null : List.of(classes));
 	}
 	
-	public static Map<String,String> buildConfig(Set<Class<?>> classes) {
+	public static Map<String,String> buildConfig(List<Class<?>> classes) {
 		Map<String, String> config = new HashMap<>();
 		config.put("quarkus.hibernate-orm.sql-load-script", String.format("sql/%s.sql", classes.stream().map(klass -> klass.getSimpleName().toLowerCase()).collect(Collectors.joining("-"))));
 		config.put("quarkus.http.test-port", "0");
@@ -29,6 +30,6 @@ public interface Profile {
 	}
 	
 	public static Map<String,String> buildConfig(Class<?>...classes) {
-		return buildConfig(classes == null || classes.length == 0 ? null : new LinkedHashSet<Class<?>>(List.of(classes)));
+		return buildConfig(classes == null || classes.length == 0 ? null : new ArrayList<Class<?>>(List.of(classes)));
 	}
 }
