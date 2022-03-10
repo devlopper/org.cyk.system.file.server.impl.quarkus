e61c13a30ae30cc5eea3cc4e2b47878f3dd7e603
devlopper <kycdev@gmail.com>
2022-03-10 11:07:46 +0000

    commit

--- src/main/java/org/cyk/system/file/server/api/service/FileDto.java
+++ src/main/java/org/cyk/system/file/server/api/service/FileDto.java
@@ -4,12 +4,12 @@ public interface FileDto extends org.cyk.utility.service.entity.IdentifiableSyst
 
 	String JSON_IDENTIFIER = "identifier";
 	String JSON_DOWNLOAD_LINK = "download_link";
-	String JSON_UNIFORM_RESOURCE_LOCATOR = "uniformResourceLocator";
+	String JSON_UNIFORM_RESOURCE_LOCATOR = "uniform_resource_locator";
 	String JSON_NAME = "name";
 	String JSON_EXTENSION = "extension";
-	String JSON_MIME_TYPE = "mimeType";
+	String JSON_MIME_TYPE = "mime_type";
 	String JSON_SIZE = "size";
 	String JSON_SHA1 = "sha1";
-	String JSON_NAME_AND_EXTENSION = "nameAndExtension";
+	String JSON_NAME_AND_EXTENSION = "name_and_extension";
 	String JSON_TEXT = "text";
 }
